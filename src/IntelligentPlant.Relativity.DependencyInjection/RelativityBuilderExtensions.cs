6983e2e587886df450edcbc490eb05191ed34477
Graham Watts <grahamcharleswatts@gmail.com>
2024-02-28 10:31:01 +0200

    Dependency injection support
    The IntelligentPlant.Relativity.DependencyInjection project contains methods for registering a singleton `IRelativityParserFactory` with the DI container, and for registering additional parser configurations not specified in the well-known parser definitions.

---
+++ src/IntelligentPlant.Relativity.DependencyInjection/RelativityBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntelligentPlant.Relativity.DependencyInjection {
+
+    /// <summary>
+    /// Extension methods for <see cref="IRelativityBuilder"/>.
+    /// </summary>
+    public static class RelativityBuilderExtensions {
+
+        /// <summary>
+        /// Adds a Relativity parser configuration.
+        /// </summary>
+        /// <param name="builder">
+        ///   The builder.
+        /// </param>
+        /// <param name="configuration">
+        ///   The parser configuration to add.
+        /// </param>
+        /// <returns>
+        ///   The builder.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="configuration"/> is <see langword="null"/>.
+        /// </exception>
+        public static IRelativityBuilder AddParserConfiguration(this IRelativityBuilder builder, RelativityParserConfiguration configuration) {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            if (configuration == null) {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            builder.Services.AddSingleton(configuration);
+
+            return builder;
+        }
+
+    }
+}
