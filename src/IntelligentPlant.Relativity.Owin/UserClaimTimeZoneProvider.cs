56c5ef266f863a8236d9033eab96cee074af1b57
Graham Watts <grahamcharleswatts@gmail.com>
2024-02-28 11:11:12 +0200

    OWIN support
    IntelligentPlant.Relativity.Owin defines middleware for setting `RelativityParser.Current` for an incoming HTTP request.
    
    The time zone for the request can be set via query string, cookie, request header, user claim, or a custom mechanism.

---
+++ src/IntelligentPlant.Relativity.Owin/UserClaimTimeZoneProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Owin;
+
+namespace IntelligentPlant.Relativity.Owin {
+
+    /// <summary>
+    /// <see cref="TimeZoneProvider"/> that uses a claim from the current user to determine the 
+    /// time zone to use.
+    /// </summary>
+    public sealed class UserClaimTimeZoneProvider : TimeZoneProvider {
+
+        /// <summary>
+        /// The default claim type to use for the time zone.
+        /// </summary>
+        public const string DefaultClaimType = "timezone";
+
+        /// <summary>
+        /// The claim type to use for the time zone.
+        /// </summary>
+        public string ClaimType { get; }
+
+
+        /// <summary>
+        /// Creates a new <see cref="UserClaimTimeZoneProvider"/> instance.
+        /// </summary>
+        /// <param name="claimType">
+        ///   The claim type to use for the time zone. If <see langword="null"/> or white space, 
+        ///   <see cref="DefaultClaimType"/> will be used.
+        /// </param>
+        public UserClaimTimeZoneProvider(string? claimType = null) {
+            ClaimType = string.IsNullOrWhiteSpace(claimType)
+                ? DefaultClaimType
+                : claimType!;
+        }
+
+
+        /// <inheritdoc/>
+        public override Task<TimeZoneInfo?> GetTimeZoneAsync(IOwinContext context) {
+            if (context.Authentication.User == null || !context.Authentication.User.Identities.All(x => x.IsAuthenticated)) {
+                return Task.FromResult<TimeZoneInfo?>(null);
+            }
+
+            var claim = context.Authentication.User.FindFirst(ClaimType);
+            if (!string.IsNullOrWhiteSpace(claim?.Value)) {
+                try {
+                    var tz = TimeZoneInfo.FindSystemTimeZoneById(claim!.Value);
+                    return Task.FromResult<TimeZoneInfo?>(tz);
+                }
+                catch { }
+            }
+
+            return Task.FromResult<TimeZoneInfo?>(null);
+        }
+    }
+}
