62c911f794522e6c400b2d98a64bcb23c2410a11
Graham Watts <graham.watts@intelligentplant.com>
2024-03-01 10:31:29 +0200

    Always use requested culture when returning a parser (#11)
    * Always use requested parser culture
    
    When a parser is requested for a culture that does not have a registered parser configuration, return a parser that uses the base time and time offset keywords for the invariant parser but uses the requested culture for parsing absolute timestamps and relative offset quantities.
    
    * Add simple cache to RegexHelper
    
    RegexHelper is now resonsible for creating Regex instances instead of just the regex patterns. It maintains an internal cache (ConcurrentDictionary<string, Regex>) of regex instances that it has created and reuses the instances where possible.
    
    The cache key is derived from the parser keywords for the pattern and the decimal separator for the culture that the pattern is being created for. This allows us to create e.g. a relative timestamp pattern that uses invariant keywords but uses a comma as a decimal separator for non-English cultures.
    
    * Cache parser configurations, not parser instances
    
    RelativityParserFactory now caches RelativityParserConfiguration objects rather than IRelativityParser instances.
    
    * Always verify parser culture in unit tests

--- src/IntelligentPlant.Relativity/Internal/RegexHelper.cs
+++ src/IntelligentPlant.Relativity/Internal/RegexHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace IntelligentPlant.Relativity.Internal {
@@ -15,6 +17,12 @@ namespace IntelligentPlant.Relativity.Internal {
         /// </summary>
         private static readonly string[] s_regexSpecialCharacterEscapes = { @"\\", @"\.", @"\$", @"\^", @"\{", @"\[", @"\(", @"\|", @"\)", @"\*", @"\+", @"\?" };
 
+        /// <summary>
+        /// Holds precompiled regular expressions so that we only create an entry once for each 
+        /// combination of culture and keywords.
+        /// </summary>
+        private static readonly ConcurrentDictionary<string, Regex> s_regexCache = new ConcurrentDictionary<string, Regex>(StringComparer.OrdinalIgnoreCase);
+
 
         /// <summary>
         /// Escapes any characters in the specified string that are special characters in regular 
@@ -28,6 +36,81 @@ namespace IntelligentPlant.Relativity.Internal {
         /// </returns>
         private static string EscapeRegexSpecialCharacters(string s) {
             return s_regexSpecialCharacterEscapes.Aggregate(s, (current, specialCharacter) => Regex.Replace(current, specialCharacter, specialCharacter));
+        }
+
+
+        /// <summary>
+        /// Gets or creates a regular expression for parsing durations.
+        /// </summary>
+        /// <param name="timeOffsetSettings">
+        ///   The time offset settings.
+        /// </param>
+        /// <param name="cultureInfo">
+        ///   The culture to use for parsing numbers.
+        /// </param>
+        /// <returns>
+        ///   A regular expression for parsing durations.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="timeOffsetSettings"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="cultureInfo"/> is <see langword="null"/>.
+        /// </exception>
+        internal static Regex GetOrCreateDurationRegex(RelativityTimeOffsetSettings timeOffsetSettings, CultureInfo cultureInfo) {
+            if (timeOffsetSettings == null) {
+                throw new ArgumentNullException(nameof(timeOffsetSettings));
+            }
+            if (cultureInfo == null) {
+                throw new ArgumentNullException(nameof(cultureInfo));
+            }
+
+            // Key consists of the time offset settings and the decimal separator for the culture.
+            var decimalSeparator = cultureInfo.NumberFormat?.NumberDecimalSeparator ?? CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator;
+            var key = $"to:{Convert.ToBase64String(Encoding.UTF8.GetBytes(timeOffsetSettings.ToString()))}:{decimalSeparator}";
+            return s_regexCache.GetOrAdd(key, _ => CompileRegex(CreateDurationRegexPattern(timeOffsetSettings, cultureInfo)));
+        }
+
+
+        /// <summary>
+        /// Gets or creates a regular expression for parsing relative timestamps.
+        /// </summary>
+        /// <param name="baseTimeSettings">
+        ///   The base time settings.
+        /// </param>
+        /// <param name="timeOffsetSettings">
+        ///   The time offset settings.
+        /// </param>
+        /// <param name="cultureInfo">
+        ///   The culture to use for parsing numbers.
+        /// </param>
+        /// <returns>
+        ///   A regular expression for parsing relative timestamps.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="baseTimeSettings"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="timeOffsetSettings"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="cultureInfo"/> is <see langword="null"/>.
+        /// </exception>
+        internal static Regex GetOrCreateRelativeDateTimeRegex(RelativityBaseTimeSettings baseTimeSettings, RelativityTimeOffsetSettings timeOffsetSettings, CultureInfo cultureInfo) {
+            if (baseTimeSettings == null) {
+                throw new ArgumentNullException(nameof(baseTimeSettings));
+            }
+            if (timeOffsetSettings == null) {
+                throw new ArgumentNullException(nameof(timeOffsetSettings));
+            }
+            if (cultureInfo == null) {
+                throw new ArgumentNullException(nameof(cultureInfo));
+            }
+
+            // Key consists of the base time settings, the time offset settings and the decimal separator for the culture.
+            var decimalSeparator = cultureInfo.NumberFormat?.NumberDecimalSeparator ?? CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator;
+            var key = $"bt:{Convert.ToBase64String(Encoding.UTF8.GetBytes(baseTimeSettings.ToString()))}:{Convert.ToBase64String(Encoding.UTF8.GetBytes(timeOffsetSettings.ToString()))}:{decimalSeparator}";
+            return s_regexCache.GetOrAdd(key, _ => CompileRegex(CreateRelativeDateTimeRegexPattern(baseTimeSettings, timeOffsetSettings, cultureInfo)));
         }
 
 
@@ -49,14 +132,7 @@ namespace IntelligentPlant.Relativity.Internal {
         /// <exception cref="ArgumentNullException">
         ///   <paramref name="cultureInfo"/> is <see langword="null"/>.
         /// </exception>
-        internal static string CreateDurationRegexPattern(RelativityTimeOffsetSettings timeOffsetSettings, CultureInfo cultureInfo) {
-            if (timeOffsetSettings == null) {
-                throw new ArgumentNullException(nameof(timeOffsetSettings));
-            }
-            if (cultureInfo == null) {
-                throw new ArgumentNullException(nameof(cultureInfo));
-            }
-
+        private static string CreateDurationRegexPattern(RelativityTimeOffsetSettings timeOffsetSettings, CultureInfo cultureInfo) {
             var timeSpanUnits = new[] {
                 timeOffsetSettings.Weeks,
                 timeOffsetSettings.Days,
@@ -87,17 +163,31 @@ namespace IntelligentPlant.Relativity.Internal {
         }
 
 
-        internal static string CreateRelativeDateTimeRegexPattern(RelativityBaseTimeSettings baseTimeSettings, RelativityTimeOffsetSettings timeOffsetSettings, CultureInfo cultureInfo) {
-            if (baseTimeSettings == null) {
-                throw new ArgumentNullException(nameof(baseTimeSettings));
-            }
-            if (timeOffsetSettings == null) {
-                throw new ArgumentNullException(nameof(timeOffsetSettings));
-            }
-            if (cultureInfo == null) {
-                throw new ArgumentNullException(nameof(cultureInfo));
-            }
-            
+        /// <summary>
+        /// Creates a regular expression pattern for parsing relative timestamps.
+        /// </summary>
+        /// <param name="baseTimeSettings">
+        ///   The base time settings.
+        /// </param>
+        /// <param name="timeOffsetSettings">
+        ///   The time offset settings.
+        /// </param>
+        /// <param name="cultureInfo">
+        ///   The culture to use for parsing numbers.
+        /// </param>
+        /// <returns>
+        ///   A regular expression pattern for parsing durations.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="baseTimeSettings"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="timeOffsetSettings"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="cultureInfo"/> is <see langword="null"/>.
+        /// </exception>
+        private static string CreateRelativeDateTimeRegexPattern(RelativityBaseTimeSettings baseTimeSettings, RelativityTimeOffsetSettings timeOffsetSettings, CultureInfo cultureInfo) {
             var baseTimes = new[] {
                 baseTimeSettings.CurrentYear,
                 baseTimeSettings.CurrentMonth,
@@ -159,6 +249,20 @@ namespace IntelligentPlant.Relativity.Internal {
                     ),
                 @"))?\s*$"
             );
+        }
+
+
+        /// <summary>
+        /// Creates a compiled regular expression for the specified pattern.
+        /// </summary>
+        /// <param name="pattern">
+        ///   The pattern.
+        /// </param>
+        /// <returns>
+        ///   The compiled regular expression.
+        /// </returns>
+        private static Regex CompileRegex(string pattern) {
+            return new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Compiled, TimeSpan.FromSeconds(1));
         }
 
     }
