24dea9685b65fb2514eeac0904ef374f3c728190
Graham Watts <grahamcharleswatts@gmail.com>
2024-03-01 10:04:59 +0200

    Use RegexHelper to create regexes
    Manage regex creation using RegexHelper instead of allowing Regex instances to be passed to Parser

--- src/IntelligentPlant.Relativity/Internal/Parser.cs
+++ src/IntelligentPlant.Relativity/Internal/Parser.cs
@@ -18,13 +18,11 @@ namespace IntelligentPlant.Relativity.Internal {
         /// <param name="timeZone">
         ///   The parser time zone.
         /// </param>
-        internal Parser(RelativityParserConfiguration config, TimeZoneInfo? timeZone) : base(
+        internal Parser(RelativityParserConfiguration config, TimeZoneInfo? timeZone) : this(
             config?.CultureInfo!, 
             timeZone ?? TimeZoneInfo.Local, 
             config?.BaseTimeSettings!, 
-            config?.TimeOffsetSettings!,
-            CompileRegex(RegexHelper.CreateRelativeDateTimeRegexPattern(config?.BaseTimeSettings!, config?.TimeOffsetSettings!, config?.CultureInfo!)),
-            CompileRegex(RegexHelper.CreateDurationRegexPattern(config?.TimeOffsetSettings!, config?.CultureInfo!))
+            config?.TimeOffsetSettings!
         ) { }
 
 
@@ -43,41 +41,19 @@ namespace IntelligentPlant.Relativity.Internal {
         /// <param name="timeOffsetSettings">
         ///   The time offset keyword settings.
         /// </param>
-        /// <param name="relativeDateTimeRegex">
-        ///   The regular expression for parsing relative date and time strings.
-        /// </param>
-        /// <param name="durationRegex">
-        ///   The regular expression for parsing duration strings.
-        /// </param>
         internal Parser(
             CultureInfo cultureInfo,
             TimeZoneInfo timeZone,
             RelativityBaseTimeSettings baseTimeSettings,
-            RelativityTimeOffsetSettings timeOffsetSettings,
-            Regex relativeDateTimeRegex,
-            Regex durationRegex
+            RelativityTimeOffsetSettings timeOffsetSettings
         ) : base(
-            cultureInfo,
-            timeZone,
-            baseTimeSettings,
-            timeOffsetSettings,
-            relativeDateTimeRegex,
-            durationRegex
+            cultureInfo ?? throw new ArgumentNullException(nameof(cultureInfo)),
+            timeZone ?? throw new ArgumentNullException(nameof(timeZone)),
+            baseTimeSettings ?? throw new ArgumentNullException(nameof(baseTimeSettings)),
+            timeOffsetSettings ?? throw new ArgumentNullException(nameof(timeOffsetSettings)),
+            RegexHelper.GetOrCreateRelativeDateTimeRegex(baseTimeSettings!, timeOffsetSettings!, cultureInfo!),
+            RegexHelper.GetOrCreateDurationRegex(timeOffsetSettings!, cultureInfo!)
         ) { }
-
-
-        /// <summary>
-        /// Creates a compiled regular expression for the specified pattern.
-        /// </summary>
-        /// <param name="pattern">
-        ///   The pattern.
-        /// </param>
-        /// <returns>
-        ///   The compiled regular expression.
-        /// </returns>
-        private static Regex CompileRegex(string pattern) {
-            return new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Compiled, TimeSpan.FromSeconds(1));
-        }
 
     }
 }
