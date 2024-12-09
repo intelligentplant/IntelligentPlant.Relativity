91b4a3d19be83883c5124c962fd44b7fe870a1a9
Graham Watts <graham.watts@intelligentplant.com>
2024-12-09 12:43:55 +0200

    Convert TimeSpan to duration string (#16)
    * Convert TimeSpan to duration string
    
    Adds a ConvertToDuration extension method to RelativityParserExtensions that converts a TimeSpan into an equivalent duration string (e.g. "1.18:00:00" => "1.75D").
    
    The duration string can also optionally be rounded to the next duration unit if a more user-friendly (but less accurate) duration string is preferred (e.g. "1.18:00:00" => "2D").
    
    * Add TimeSpan => duration string conversion tests
    
    * Update build.cake
    
    Update to latest Cake extensions package
    
    * Update version.json

---
+++ test/IntelligentPlant.Relativity.Test/TimeSpanConversionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntelligentPlant.Relativity.Test {
+
+    [TestClass]
+    public class TimeSpanConversionTests {
+
+        [DataTestMethod]
+        [DataRow("1.18:00:00", false, "1.75D")]
+        [DataRow("1.18:00:00", true, "2D")]
+        [DataRow("18:30:00", false, "18.5H")]
+        [DataRow("18:30:00", true, "19H")]
+        [DataRow("00:15:45", false, "15.75M")]
+        [DataRow("00:15:45", true, "16M")]
+        [DataRow("00:00:01.250", false, "1.25S")]
+        [DataRow("00:00:01.250", true, "2S")]
+        [DataRow("00:00:00.2345678", false, "234.5678MS")]
+        [DataRow("00:00:00.2345768", true, "235MS")]
+        [DataRow("-1.18:00:00", false, "-1.75D")]
+        [DataRow("-1.18:00:00", true, "-2D")]
+        [DataRow("-18:30:00", false, "-18.5H")]
+        [DataRow("-18:30:00", true, "-19H")]
+        [DataRow("-00:15:45", false, "-15.75M")]
+        [DataRow("-00:15:45", true, "-16M")]
+        [DataRow("-00:00:01.250", false, "-1.25S")]
+        [DataRow("-00:00:01.250", true, "-2S")]
+        [DataRow("-00:00:00.2345678", false, "-234.5678MS")]
+        [DataRow("-00:00:00.2345768", true, "-235MS")]
+        public void ShouldConvertToDuration(string timeSpanLiteral, bool truncateDuration, string expectedDuration) { 
+            var timeSpan = TimeSpan.Parse(timeSpanLiteral);
+            var duration = RelativityParser.InvariantUtc.ConvertToDuration(timeSpan, truncateDuration);
+            Assert.AreEqual(expectedDuration, duration);
+        }
+
+
+    }
+
+}
