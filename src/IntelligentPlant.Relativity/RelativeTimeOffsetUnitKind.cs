5f39b45976e01dcfa0700e3b320d2e19083433ae
Graham Watts <grahamcharleswatts@gmail.com>
2024-12-10 10:19:08 +0200

    Add enums to represent duration/offset unit types

---
+++ src/IntelligentPlant.Relativity/RelativeTimeOffsetUnitKind.cs
@@ -0,0 +1,55 @@
+﻿namespace IntelligentPlant.Relativity {
+
+    /// <summary>
+    /// Describes the unit of a duration.
+    /// </summary>
+    public enum RelativeTimeOffsetUnitKind {
+
+        /// <summary>
+        /// Unspecified.
+        /// </summary>
+        Unspecified,
+
+        /// <summary>
+        /// Milliseconds.
+        /// </summary>
+        Milliseconds,
+
+        /// <summary>
+        /// Seconds.
+        /// </summary>
+        Seconds,
+
+        /// <summary>
+        /// Minutes.
+        /// </summary>
+        Minutes,
+
+        /// <summary>
+        /// Hours.
+        /// </summary>
+        Hours,
+
+        /// <summary>
+        /// Days.
+        /// </summary>
+        Days,
+
+        /// <summary>
+        /// Weeks.
+        /// </summary>
+        Weeks,
+
+        /// <summary>
+        /// Months.
+        /// </summary>
+        Months,
+
+        /// <summary>
+        /// Years.
+        /// </summary>
+        Years
+
+    }
+
+}
